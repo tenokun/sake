69aa36436ca0dace8b9b893273731d99501758a6
tenokun <ten-210.10@i.softbank.jp>
2022-08-03 13:42:26 +0900

    ブログ投稿詳細の表示

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Posts</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <h1 class="name">
+            {{ $post->name }}
+        </h1>
+        <div class="content">
+            <div class="content_post">
+                <h3>種類</h3>
+                <p>{{ $post->kind }}</p>
+                <h3>作り方</h3>
+                <p>{{ $post->body }}</p>
+            </div>
+        </div>
+        <div class="footer">
+            <a href="/">戻る</a>
+        </div>
+    </body>
+</html>
